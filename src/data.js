0dae7d5c2364b0f762fe05efce126a4527cb1cca
Pablo Ledesma Ortiz (CM) <Pledesma@copaair.com>
2016-07-25 16:01:51 -0500

    form and Data

---
+++ src/data.js
@@ -0,0 +1,52 @@
+"use strict";
+var Data = (function () {
+    function Data() {
+        this.sysCities = {
+            "fromcities": [
+                { "name": "Guatemala, GT", "id": "GUA" },
+                { "name": "Guayaquil, EC", "id": "GYE" },
+                { "name": "La Habana, CU", "id": "HAV" },
+                { "name": "Lima, PE", "id": "LIM" },
+                { "name": "Liberia, CR", "id": "LIR" },
+                { "name": "Montego Bay, JM", "id": "MBJ" },
+                { "name": "Managua, NI", "id": "MGA" },
+                { "name": "Panama, PA", "id": "PTY" },
+                { "name": "San Salvador, SV", "id": "SAL" },
+                { "name": "Santiago de Chile, CL", "id": "SCL" },
+                { "name": "Santo Domingo, DO", "id": "SDQ" },
+                { "name": "Santa Clara, CU", "id": "SNU" },
+                { "name": "Santiago de los Caballeros, DO", "id": "STI" },
+                { "name": "Tegucigalpa, HN", "id": "TGU" },
+                { "name": "Quito, EC", "id": "UIO" }
+            ],
+            "tocities": [
+                { "name": "Bucaramanga, CO", "id": "BGA" },
+                { "name": "Bogota, CO", "id": "BOG" },
+                { "name": "Cucuta, CO", "id": "CUC" },
+                { "name": "Guatemala, GT", "id": "GUA" },
+                { "name": "Guayaquil, EC", "id": "GYE" },
+                { "name": "La Habana, CU", "id": "HAV" },
+                { "name": "Lima, PE", "id": "LIM" },
+                { "name": "Liberia, CR", "id": "LIR" },
+                { "name": "Montego Bay, JM", "id": "MBJ" },
+                { "name": "Medellin, CO", "id": "MDE" },
+                { "name": "Managua, NI", "id": "MGA" },
+                { "name": "Panama, PA", "id": "PTY" },
+                { "name": "San Salvador, SV", "id": "SAL" },
+                { "name": "Santiago de Chile, CL", "id": "SCL" },
+                { "name": "Santo Domingo, DO", "id": "SDQ" },
+                { "name": "Santa Clara, CU", "id": "SNU" },
+                { "name": "Santiago de los Caballeros, DO", "id": "STI" },
+                { "name": "Tegucigalpa, HN", "id": "TGU" },
+                { "name": "Quito, EC", "id": "UIO" }
+            ],
+            departure_date: departure_date,
+            return_date: return_date,
+            // Mensajes de error para el booking
+            msj: msj
+        };
+    }
+    return Data;
+}());
+exports.Data = Data;
+//# sourceMappingURL=data.js.map
