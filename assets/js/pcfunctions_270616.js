6f615723154593ace0d4aa49c29f811e5f2b5f49
Pablo Ledesma Ortiz (CM) <Pledesma@copaair.com>
2016-07-25 15:07:06 -0500

    actualización de libreria jqueryui

--- assets/js/pcfunctions_270616.js
+++ assets/js/pcfunctions_270616.js
@@ -1,11 +1,17 @@
 // JavaScript Document
 $(function() {
-	
+
+	//Construye el formulario
 	$.each(sysCities.fromcities, function (index, value) {
 		$("#DESDE").append('<option value="'+value.id+'">'+value.name+'</option>');
 	});
 
+	$.each(sysCities.tocities, function (index, value) {
+		$("#HACIA").append('<option value="'+value.id+'">'+value.name+'</option>');
+	});
+
 	var storeFront = '';
+	//Agrega eventos al selector
 	$("#DESDE").on("change", function(){
 		var selected = $(this).val();
 		$.each( sysCities.fromcities, function(index, value){
@@ -26,11 +32,6 @@ $(function() {
 		buscarVuelos( storeFront );
 	});
 
-	$.each(sysCities.tocities, function (index, value) {
-		$("#HACIA").append('<option value="'+value.id+'">'+value.name+'</option>');
-	});
-
-	
 	$.datepicker.setDefaults($.datepicker.regional['es']);
 	var dates = $( "#from, #to" ).datepicker({
 		dateFormat: 'dd/mm/yy',
---
+++ src/form.js
@@ -0,0 +1,8 @@
+"use strict";
+var Form = (function () {
+    function Form() {
+    }
+    return Form;
+}());
+exports.Form = Form;
+//# sourceMappingURL=form.js.map
