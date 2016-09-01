f47c24eaf501414dc9d4bcfcfea3838b8a64c5fe
= <=>
2016-08-31 19:42:31 -0500

    finalización lp caracas

---
+++ assets/js/origenes_destinos_fechas_caracas.js
@@ -0,0 +1,10 @@
+var sysCities = {
+    "fromcities": [
+        {"name":"Caracas, VE", "id":"CCS"}
+    ],
+    "tocities": [
+        {"name":"Washington Dulles, US", "id":"IAD"}
+    ],
+},
+departure_date = new Date(2016, 09 - 1, 01),
+return_date = new Date(2016, 12 - 1, 08);
