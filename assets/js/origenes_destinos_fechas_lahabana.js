d07e16e2fa86af1feed9a935aa53601c28905f55
Pablo Ledesma <pabloledes83@gmail.com>
2016-08-31 22:31:45 -0500

    go live for guayaquil and la habana

---
+++ assets/js/origenes_destinos_fechas_lahabana.js
@@ -0,0 +1,18 @@
+var sysCities = {
+    "fromcities": [
+         {"name":"La Habana, CU", "id":"HAV"}
+                
+             ],
+                        
+            "tocities": [
+            
+		    {"name":"Barranquilla, CO", "id":"BAQ"},
+			{"name":"Quito, EC", "id":"UIO"}
+		
+           
+     ]
+	},
+
+      departure_date = new Date(2016, 09 - 1, 01),
+      return_date = new Date(2016, 12 - 1, 08);
+
