89cc7dac7de3067df949a84c6cbf6516cd2a44ff
= <=>
2016-08-31 20:40:01 -0500

    actualización de estilos en el lp de san-jose | finalización del lp de david

---
+++ assets/js/origenes_destinos_fechas_david.js
@@ -0,0 +1,12 @@
+var sysCities = {
+    "fromcities": [
+		{"name":"David, PA", "id":"DAV"}
+    ],
+    "tocities": [
+    	{"name":"Boston, US", "id":"BOS"},
+    	{"name":"Orlando, US", "id":"MCO"},
+    	{"name":"Los Vegas, US", "id":"LAS"}	
+    ],
+},
+departure_date = new Date(2016, 09 - 1, 01),
+return_date = new Date(2016, 12 - 1, 08);
