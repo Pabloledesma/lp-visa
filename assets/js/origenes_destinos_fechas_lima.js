17fac7bbbd9822cba27fff1feac4c7a9a44c784e
Pablo Ledesma <pabloledes83@gmail.com>
2016-09-01 07:37:31 -0500

    finishing lima

---
+++ assets/js/origenes_destinos_fechas_lima.js
@@ -0,0 +1,25 @@
+var sysCities = {
+    "fromcities": [
+        {"name":"Lima, PE", "id":"LIM"}
+                
+             ],
+                        
+            "tocities": [
+            
+      {"name":"Boston, US", "id":"BOS"},
+	  {"name":"Washington Dulles, US", "id":"IAD"},
+	  {"name":"La Habana, CU", "id":"HAV"},
+	  {"name":"Montego Bay, JM", "id":"MBJ"},
+	  {"name":"Santo Domingo, DO", "id":"SDQ"},
+	  {"name":"San Juan, PR", "id":"SJU"},
+	   {"name":"Canc&uacute;n, MX", "id":"CUN"},
+		{"name":"St Maarten, AN", "id":"SXM"}
+    
+			
+           
+     ]
+	},
+
+      departure_date = new Date(2016, 09 - 1, 01),
+      return_date = new Date(2016, 12 - 1, 08);
+
