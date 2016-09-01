2829b243ea0a2c6bae1254648d30e2ac2f34514e
Pablo Ledesma <pabloledes83@gmail.com>
2016-09-01 09:10:56 -0500

    actualización del lp de boston

---
+++ assets/js/origenes_destinos_fechas_chicago.js
@@ -0,0 +1,16 @@
+var sysCities = {
+    "fromcities": [
+		{"name":"Chicago, US", "id":"ORD"}
+	
+    ],
+
+    "tocities": [
+		{"name":"Quito, EC", "id":"UIO"}
+    ],
+},
+departure_date = new Date(2016, 09 - 1, 01),
+return_date = new Date(2016, 12 - 1, 08);
+
+
+
+
