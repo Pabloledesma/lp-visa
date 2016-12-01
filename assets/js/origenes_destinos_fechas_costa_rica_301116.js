3463c88b347acc13ba49ba9c9aa522aa4a309e9a
Pablo Ledesma <pabloledes83@gmail.com>
2016-11-30 21:05:52 -0500

    lp de costa rica, guatemala, managua y san jose

---
+++ assets/js/origenes_destinos_fechas_costa_rica_301116.js
@@ -0,0 +1,13 @@
+var sysCities = {
+    "fromcities": [
+        {"name":"Managua, NI", "id":"MGA"}
+    ],
+                        
+    "tocities": [
+		{"name":"San José, CR", "id":"SJO"}
+    ]
+},
+
+	departure_date = new Date(2016, 12 - 1, 10),
+	return_date = new Date(2017, 6 - 1, 30);
+
